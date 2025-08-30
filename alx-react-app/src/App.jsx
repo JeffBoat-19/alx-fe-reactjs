70e731b22b992c1a748502c7a7ec2b1d25fb5186
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-08-30 14:45:14 +0000

    Added header, footer and Maincontent

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage.js";
+import WelcomeMessage from "./Components/WelcomeMessage.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import MainContent from "./components/MainContent.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
