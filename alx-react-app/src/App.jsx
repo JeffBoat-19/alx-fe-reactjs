c93332fb21201e425946e7d79cc925dc4a076019
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-08-30 14:28:03 +0000

    Fixed error

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import WelcomeMessage from "./Components/WelcomeMessage";
+import WelcomeMessage from "./Components/WelcomeMessage.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
