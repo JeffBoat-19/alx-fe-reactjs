b5b5d032aab938811ef3a10c613479b55c81e8dd
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-08-31 16:12:18 +0000

    Fixed WelcomeMessage in app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <>
+      <WelcomeMessage />
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
