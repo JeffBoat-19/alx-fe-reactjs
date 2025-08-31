2e77f277b05ad421d8b02d713dc6f7fec378e777
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-08-31 16:08:47 +0000

    Fixed props error

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
-function UserProfile(user) {
+function UserProfile(props) {
   return (
     <div>
-      <h2>{user.name}</h2>
-      <p>Age: {user.age}</p>
-      <p>Bio: {user.bio}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 }
