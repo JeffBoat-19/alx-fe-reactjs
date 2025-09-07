768c173fb40aa2b358a57e93188abff1b494c776
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-09-07 23:12:07 +0000

    Fixed Maincontent sentence

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent() {
   return (
     <main style={{ padding: "10px", backgroundColor: "#f0f0f0" }}>
       <h2>Main Content</h2>
-      <p>This is where the main content goes.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
