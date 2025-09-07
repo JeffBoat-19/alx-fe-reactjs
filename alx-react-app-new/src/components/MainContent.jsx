95a4538bc69948e1f85eba0390ddd6b0486d76b4
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-09-07 23:07:24 +0000

    Initaila commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,19 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#eef2f3",
+        minHeight: "200px",
+      }}
+    >
+      <h2 style={{ color: "darkslategray" }}>Welcome to the City Explorer</h2>
+      <p style={{ lineHeight: "1.6" }}>
+        Here you can learn about different cities, their cultures, and why they
+        are special.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
