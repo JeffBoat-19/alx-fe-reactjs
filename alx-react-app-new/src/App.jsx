b9c1fb0fb504090ac8ffd000762b816e03021186
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-09-11 12:41:37 +0000

    Add alx-react-app-props project

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import WelcomeMessage from "./Components/WelcomeMessage.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import MainContent from "./components/MainContent.jsx";
+import UserProfile from "./components/UserProfile.jsx";
+import Counter from "./components/Counter";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      <p>Footer Section</p>
+    </footer>
+  );
+}
+
+export default Footer;
