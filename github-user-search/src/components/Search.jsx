cf98a245ed4b541c61e3e3af7f20983408a126c2
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-09-26 03:18:21 +0000

    Fixed error message in seach.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ function Search() {
       const data = await fetchUserData(username);
       setUserData(data);
     } catch (err) {
-      setError("Looks like we can’t find the user 😢");
+      setError("Looks like we can’t find the user");
     } finally {
       setLoading(false);
     }
