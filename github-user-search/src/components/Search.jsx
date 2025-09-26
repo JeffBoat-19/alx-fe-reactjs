c568a953083d9f947e535eff43bd214b3c2c79bc
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-09-26 03:15:43 +0000

    Added services and a component

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { fetchUserData } from "../services/githubService";
+
+function Search() {
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSearch = async (e) => {
+    e.preventDefault(); // prevent page refresh
+    setLoading(true);
+    setError("");
+    setUserData(null);
+
+    try {
+      const data = await fetchUserData(username);
+      setUserData(data);
+    } catch (err) {
+      setError("Looks like we can’t find the user 😢");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      {/* Search Form */}
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Enter GitHub username..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {/* Conditional Rendering */}
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {userData && (
+        <div>
+          <img src={userData.avatar_url} alt={userData.login} width="100" />
+          <h2>{userData.name || userData.login}</h2>
+          <a href={userData.html_url} target="_blank">
+            View Profile
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Search;
